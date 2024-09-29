4d26a2a2c2e0c808799bf541f31ae6dc4aa93b47
Santiago Martinez <smartinez9626@gmail.com>
2024-09-28 21:32:27 -0500

    Refactor ProjectView component to include dynamic routing and improve table layout

--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -1,26 +1,67 @@
 <template>
-  <BreadCrumbs :name="project?.name ?? 'No name'"></BreadCrumbs>
+  <div class="w-full">
+    <section class="m-2">
+      <BreadCrumbs :name="project?.name ?? 'No name'"></BreadCrumbs>
+    </section>
+    <section class="m-2">
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th class="w-14">Completada</th>
+              <th>Tarea</th>
+              <th>Completada en</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="hover">
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+            </tr>
+            <tr class="hover">
+              <th>2</th>
+              <td>
+                <input
+                  type="text"
+                  class="input input-primary w-full opacity-60 transition-all hover:opacity-100 focus:opacity-100"
+                  placeholder="Nueva tarea"
+                />
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+
 import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
 import { useProjectsStore } from '../store/projects.store';
-import { ref, watch } from 'vue';
 import type { Project } from '../interfaces/project.interface';
+
 interface Props {
   id: string;
 }
 
+const router = useRouter();
 const props = defineProps<Props>();
-
 const projectStore = useProjectsStore();
-
 const project = ref<Project | null>();
 
 watch(
   () => props.id,
   () => {
     project.value = projectStore.projectList.find((project) => project.id === props.id) ?? null;
+    if (!project.value) {
+      router.replace('/projects');
+    }
   },
   {
     immediate: true,
